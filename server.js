9ff97c1887beb0194ad372d87a70321bba4d9aab
Sajeel Aalam <imsajeel.dev@gmail.com>
2020-05-21 17:15:30 +0500

    / route added

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PeopleRoutes = require("./routes/peoples");
 const app = express();
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
 app.use("/peoples", PeopleRoutes);
 
 app.listen(3000);
